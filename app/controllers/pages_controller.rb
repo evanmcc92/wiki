3b685a7715b3a22d7f8f9839d08ad02245d5f11c
Evan McCullough <webmaster@evanamccullough.com>
2014-01-11 00:49:30 -0500

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -49,7 +49,7 @@ class PagesController < ApplicationController
   end
 
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
 
     @page = Page.find(params[:id])
 
